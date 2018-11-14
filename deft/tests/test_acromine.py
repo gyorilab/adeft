c9c512d13ad18581cabe4fca4e78404663c95ea7
Albert Steppi <albert_steppi@hms.harvard.edu>
2018-11-14 16:20:15 -0500

    add empty test

---
+++ deft/tests/test_acromine.py
@@ -0,0 +1,18 @@
+import unittest
+from nose.plugins.attrib import attr
+
+
+def test_add():
+    return
+
+
+def test_consume():
+    return
+
+
+def test_top():
+    return
+
+
+def test__get_candiates():
+    return
