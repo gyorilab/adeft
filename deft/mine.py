242f30d2ee3673378fe238e67e5393f1295e5166
Albert Steppi <albert_steppi@hms.harvard.edu>
2018-11-15 16:11:16 -0500

    refactored and added documentation to ContinuousMiner

--- deft/mine.py
+++ deft/mine.py
@@ -64,7 +64,7 @@ _greek_alphabet = {
 
 
 class SnowCounter(object):
-    """Wraps EnglishStemmer.
+    """Wraps the nltk.snow EnglishStemmer.
 
     Keeps track of the number of times words have been mapped to particular
     stems by the wrapped stemmer. The acromine algorithm works with stemmed
@@ -78,7 +78,7 @@ class SnowCounter(object):
         Contains the count of the number of times a particular word has been
         mapped to from a particular stem by the wrapped stemmer. Of the form
         counts[stem:str][word:str] = count:int
-"""
+    """
     def __init__(self):
         self.__snow = EnglishStemmer()
         self.counts = defaultdict(lambda: defaultdict(int))
@@ -151,19 +151,95 @@ class ContinuousMiner(object):
         Dictionary mapping candidate longforms to their likelihoods as
         produced by the acromine algorithm
 
-    _snow: :py:class:`deft.acromine.SnowCounter`
+    _snow: :py:class:`deft.mine.SnowCounter`
         English stemmer that keeps track of counts of the number of times a
         given word has been mapped to a given stem
     """
-    def __init__(self, shortform, stop_words=set([])):
+    def __init__(self, shortform, stop_words=_stop):
         self.shortform = shortform
         self._internal_dict = {}
         self._longforms = {}
         self._snow = SnowCounter()
         self.stop_words = stop_words
 
+    class LikelihoodTracker(object):
+        """ Stores meta data associated with a candidate longform
+
+        Contains the current likelihood for the candidate longform as well
+        as all information needed to calculate it. Allows for updating the
+        likelihood in the face of new information.
+
+        In the remainder, a candidate longform t is a child of a candidate
+        longform c if t can be obtained from c by prepending a single token.
+
+        Parameters
+        ----------
+        longform: list of str
+            List of tokens within a candidate longform in reverse order.
+            For the candidate longform "in the endoplasmic reticulum" the
+            list will take the form ["reticulum", "endoplasmic", "the", "in"],
+            ignoring that the tokens should actually be stemmed.
+
+        Attributes
+        ----------
+
+        longform: list of str
+            Explained above
+
+        count: int
+            Current co-occurence frequency of candidate longform with shortform
+
+        sum_ft: int
+            Sum of the co-occurence frequencies of all previously observed
+            candidate longforms that are children of the associated candidate
+            longform (longforms that can be obtained by prepending
+            one token to the associated candidate longform).
+
+        sum_ft2: int
+            Sum of the squares of the co-occurence freqencies of all previously
+            observed candidate longforms that are children of the associated
+            longform.
+        LH: float
+            Likelihood of the associated candidate longform. It is given by
+            count*log(len(longform)) - sum_ft/sum_ft**2
+            See
+
+            [Okazaki06] Naoaki Okazaki and Sophia Ananiadou. "Building an
+            abbreviation dicationary using a term recognition approach".
+            Bioinformatics. 2006. Oxford University Press.
+
+            for more information
+        """
+        def __init__(self, longform):
+            self.longform = longform
+            self.count = 1
+            self.sum_ft = 0
+            self.sum_ft2 = 0
+            self.LH = np.log(len(longform))
+
+        def increment_count(self):
+            """Update count and likelihood when observing a longform again"""
+            self.count += 1
+            self.LH += np.log(len(self.longform))
+
+        def update_likelihood(self, count):
+            """Update likelihood when observing a child of associated longform
+
+            Update when observing a candidate longform which can be obtained
+            by prepending one token to the associated longform.
+
+            Parameters
+            ----------
+            count: int
+                Current co-occurence frequency of child longform with shortform
+            """
+            self.sum_ft += 1
+            self.sum_ft2 += 2*count - 1
+            self.LH += (self.sum_ft - 1)/(self.sum_ft2 - count - 1)
+            self.LH -= self.sum_ft/self.sum_ft2
+
     def consume(self, texts):
-        """Consume a corpus of texts and use them to train the Acromine
+        """Consume a corpus of texts and use them to train the miner
 
         Each text is tokenized into sentences. Sentences are identified that
         contain the pattern f'({self.shortform}). These sentences are then
@@ -182,37 +258,56 @@ class ContinuousMiner(object):
         "estrogen receptor", "receptor" would then be added to the internal
         trie.
 
-        Parameters:
+        Parameters
+        ----------
         texts: list of str
             A list of texts
         """
         # split each text into a list of sentences
         split = [sent_tokenize(text) for text in texts]
+
         # extract sentences defining shortform through standard pattern
         sentences = [sentence for text in split for sentence in text
                      if f'({self.shortform})' in sentence]
+
         # tokenize these sentences into lists of words
         tokens = [word_tokenize(sentence) for sentence in sentences]
+
         # extract maximal candidate longforms from each such sentence
         candidates = [self._get_candidates(sentence)[::-1]
                       for sentence in tokens]
+
         # add each candidate to the internal trie
         for candidate in candidates:
             self._add(candidate)
 
-    def top(self, n):
-        """Return top scoring candidates from the acromine."""
+    def top(self, limit=None):
+        """Return top scoring candidates from the mine.
 
-        output = sorted(self._longforms.items(), key=lambda x: x[1],
-                        reverse=True)[0:n]
-        output = [(tuple(self._snow.most_frequent(token)
-                         for token in longform),
-                   LH)
-                  for longform, LH in output]
-        return output
+        Parameters
+        ----------
+        limit: Optional[int]
+            Limit for the number of candidates to return. Default: None
+
+        Returns
+        ------
+        candidates: list of tuple
+            List of tuples, each containing a candidate string and its
+            likelihood score, sorted in descending order by likelihood score.
+        """
+        candidates = sorted(self._longforms.items(), key=lambda x: x[1],
+                            reverse=True)
+        if limit is not None and limit < len(candidates):
+            candidates = candidates[0:limit]
+
+        candidates = [(' '.join(self._snow.most_frequent(token)
+                                for token in longform),
+                       LH)
+                      for longform, LH in candidates]
+        return candidates
 
     def _add(self, tokens):
-        """Add a list of tokens to the internal trie and update likelihoods
+        """Add a list of tokens to the internal trie and update likelihoods.
 
         Parameters
         ----------
@@ -220,58 +315,90 @@ class ContinuousMiner(object):
             A list of tokens to add to the internal trie.
 
         """
+        # start at top of trie
         current = self._internal_trie
-        meta = {}
-        for index, token in enumerate(tokens):
+        previous = None
+        for token in tokens:
             if token not in current:
-                if index > 0:
-                    prev_lf = meta['longform']
+                # candidate longform is observed for the first time
+                # add a new entry for it in the trie
+                if previous is not None:
+                    longform = previous[0]['longform'] + (token, )
                 else:
-                    prev_lf = []
-                new = [{'count': 1,
-                        'freqt': 0,
-                        'freqt2': 0,
-                        'LH': np.log(len(prev_lf) + 1),
-                        'longform': prev_lf + [token]}, {}]
-                self._longforms[tuple(new[0]['longform'][::-1])] = \
-                    new[0]['LH']
+                    # previously unobserved candidate longform has only one
+                    # token and thus has no observed parent
+                    longform = (token, )
+                # initialize new node
+                new = [self.LikelihoodTracker(longform), {}]
+                # Add newly observed longform to the dictionary of candidates
+                self._longforms[new[0].longform[::-1]] = new[0].LH
+                # set newly observed longform to be child of current node in
+                # trie
                 current[token] = new
-                meta = new[0]
-                current = new[1]
+                # update current node to the newly formed node and set previous
+                # node
+                previous, current = current, new
             else:
-                count = current[token][0]['count']
-                current[token][0]['count'] += 1
-                current[token][0]['LH'] += \
-                    np.log(len(current[token][0]['longform']))
-                self._longforms[tuple(current[token][0]['longform'])[::-1]] = \
-                    current[token][0]['LH']
-                if index > 0:
-                    meta['freqt'] += 1
-                    meta['freqt2'] += 2*count + 1
-                    ft = meta['freqt']
-                    ft2 = meta['freqt2']
-                    meta['LH'] += (ft - 1)/(ft2 - 2*count + 1)
-                    meta['LH'] -= ft/ft2
-                    current[token][0]['longform'] = \
-                        meta['longform'] + [token]
-                    self._longforms[tuple(meta['longform'][::-1])] = \
-                        meta['LH']
-                else:
-                    current[token][0]['longform'] = [token]
-                meta = current[token][0]
-                current = current[token][1]
+                # candidate longform has been observed before
+                # update count for candidate longform and associated LH value
+                current[token][0].increment_count()
+                # Update entry for candidate longform in the candidates
+                # dictionary
+                self._longforms[current[token][0].longform[::-1]] = \
+                    current[token][0].LH
+                if previous is not None:
+                    # we are not at the top of the trie. observed candidate
+                    # has a parent
+
+                    # update likelihood of candidate's parent
+                    count = current[token][0].count
+                    previous[0].update_likelihood(count)
+                    # Update candidates dictionary
+                    self._longforms[previous[0].longform[::-1]] = \
+                        previous[0].LH
+                previous, current = current, current[token][1]
 
     def _get_candidates(self, tokens):
+        """Returns maximal candidate longform from a list of tokens.
+
+        Parameters
+        ----------
+        tokens: list of str
+            A list of tokens which that been taken from a sentence containing
+            the shortform in parentheses
+
+        Returns
+        -------
+        candidate: list of str
+            Sublist of input list containing tokens between start of sentence
+            and first occurence of the shortform in parentheses, or between
+            a stop word and the first occurence of the shortform in parentheses
+            if there is a set of stop words to exclude from longforms.
+        """
+        # Loop through tokens. The nltk word tokenizer used will split off
+        # the parentheses surrounding the shortform into separate tokens.
         for index in range(len(tokens) - 3):
             if tokens[index] == '(' and tokens[index+1] == self.shortform and \
                tokens[index+2] == ')':
-                output = [token for token in tokens[:index]
-                          if token not in string.punctuation]
-                for i, token in enumerate(output):
+                # The shortform has been found in parentheses
+
+                # Capture all tokens in the sentence up until but excluding
+                # the left parenthese containing the shortform, excluding
+                # punctuation
+                candidate = [token for token in tokens[:index]
+                             if token not in string.punctuation]
+                # If a token consists of a sole unicode greek letter, replace
+                # it with the letter spelled out in Roman characters
+                for i, token in enumerate(candidate):
                     if token in _greek_alphabet:
-                        output[i] = _greek_alphabet[token]
-                output = [self._snow.stem(token) for token in output]
-                i = len(output)-1
-                while i >= 0 and output[i] not in self.stop_words:
+                        candidate[i] = _greek_alphabet[token]
+                # Apply the snowball stemmer to each token
+                candidate = [self._snow.stem(token) for token in candidate]
+
+                # Keep only the tokens preceding the left parenthese up until
+                # but not including the first stop word
+                i = len(candidate)-1
+                while i >= 0 and candidate[i] not in self.stop_words:
                     i -= 1
-                return output[i+1:]
+                candidate = candidate[i+1:]
+                return candidate
