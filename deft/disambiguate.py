7291d6bf22967ccd621ec76271bb86c30a49b6b6
steppi <albert_steppi@hms.harvard.edu>
2019-01-14 19:01:25 -0500

    Fix logical error in if-else block
    Indentation level was set incorrectly causing deft disambiguator to fail

--- deft/disambiguate.py
+++ deft/disambiguate.py
@@ -1,11 +1,12 @@
 import logging
 
 from deft.util import contains_shortform
+from deft.recognize import LongformRecognizer
 
 logger = logging.getLogger('disambiguate')
 
 
-class Disambiguator(object):
+class DeftDisambiguator(object):
     """Disambiguates longforms in texts for a particular shortform
 
     Checks first if the standard pattern is matched in the text. If it is
@@ -22,29 +23,29 @@ class Disambiguator(object):
        recognizes longforms based on the standard pattern
     longform_classifier :  py:class:`deft.modeling.classify.LongformClassifier`
        machine learning model for disambiguating shortforms based upon context
-    
+
     Attributes
     ----------
     shortform : str
         shortform to disambiguate. This is also an attribute of both the
         recognizer and classifier.
     """
-    def __init__(self, longform_recognizer, longform_classifier):
-        self.lf_recognizer = longform_recognizer
+    def __init__(self, longform_classifier, grounding_map):
         self.lf_classifier = longform_classifier
-        assert longform_recognizer.shortform == longform_recognizer.shortform
-        assert longform_classifier.shortform == longform_classifier.shortform
-        self.shortform = longform_recognizer.shortform
+        self.shortform = longform_classifier.shortform
+        self.lf_recognizer = LongformRecognizer(self.shortform,
+                                                grounding_map)
 
     def disambiguate(self, text):
         if contains_shortform(text, self.shortform):
             longforms = self.lf_recognizer.recognize(text)
             if len(longforms) == 1:
                 return longforms.pop()
-            else:
-                prediction = self.lf_classifier.predict_proba([text])[0]
-                if not longforms:
-                    return max(prediction.iterkeys(),
-                               key=lambda key: prediction[key])
-                else:
-                    return max(prediction[label] for label in longforms)
+        else:
+            longforms = set()
+        prediction = self.lf_classifier.predict_proba([text])[0]
+        if not longforms:
+            return max(prediction.keys(),
+                       key=lambda key: prediction[key])
+        else:
+            return max(prediction[label] for label in longforms)
