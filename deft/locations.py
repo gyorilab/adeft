b99d80450155da48836619178c04144e87300575
steppi <albert_steppi@hms.harvard.edu>
2019-02-14 11:26:01 -0500

    Fix bucket url

--- deft/locations.py
+++ deft/locations.py
@@ -2,4 +2,4 @@ import os
 
 DEFT_PATH = os.path.dirname(os.path.abspath(__file__))
 MODELS_PATH = DEFT_PATH + '/models'
-S3_BUCKET_URL = 'http://bucket.s3-us-east-1.amazonaws.com'
+S3_BUCKET_URL = 'http://deft-models.s3.amazonaws.com'
