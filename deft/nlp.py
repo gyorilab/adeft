69f0101d1c69a65212d9b3cb1f17c90c6329c91a
steppi <albert_steppi@hms.harvard.edu>
2019-02-25 15:18:59 -0500

    Implement inverse tokenizer

--- deft/nlp.py
+++ deft/nlp.py
@@ -93,6 +93,37 @@ def tokenize(text):
 
 
 def untokenize(tokens):
-    """Returns inverse of the deft.nlp.word_tokenize
+    """Returns inverse of deft's word tokenizer
+
+    The inverse is inexact. For simplicity, all white space characters are
+    replaced with a space. An exact inverse is not necessary for deft's
+    purposes.
+
+    Parameters
+    ----------
+    tokens : list of tuple
+        List of tuples of the form (word, (start, end)) giving tokens
+        and coordinates as output by deft's word tokenizer
+
+    Returns
+    -------
+    output : str
+        The original string that produced the input tokens, with the caveat
+        that every white space character will be replaced with a space.
     """
-    return
+    # Edge cases: input text is empty string or only has one token
+    if len(tokens) == 0:
+        return ''
+    elif len(tokens) == 1:
+        return tokens[0][0]
+
+    # This looks kind of harry but is simple conceptually.
+    # At each step add the current token and a number of spaces determined
+    # by the coordinates of the previous token and the current token.
+    output = [tokens[0][0]] + [' ']*(tokens[1][1][0] - tokens[0][1][1] - 1)
+    for index in range(1, len(tokens)-1):
+        output.append(tokens[index][0])
+        output.extend([' ']*(tokens[index+1][1][0]
+                             - tokens[index][1][1] - 1))
+    output.append(tokens[-1][0])
+    return ''.join(output)
