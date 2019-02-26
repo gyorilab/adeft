709c9419e0f4ab13c6c5e40ff68165b2f9d4270f
steppi <albert_steppi@hms.harvard.edu>
2019-02-26 10:03:19 -0500

    Add flag in deft.download to update models if desired

--- deft/download/__main__.py
+++ deft/download/__main__.py
@@ -1,8 +1,17 @@
+import argparse
 from deft.download import download_models
 
 """
-Allows models to be downloaded from the command line with python -m deft.download
+Allows models to be downloaded from the command line with
+python -m deft.download
 
-This is useful for downloading deft models on Travis
+Use python -m deft.download --update
+to update existing models if models have changed on S3
 """
-download_models()
+
+parser = argparse.ArgumentParser(description='Download models from S3')
+parser.add_argument('--update', action='store_true',
+                    help='Update existing models if they have changed on S3')
+args = parser.parse_args()
+
+download_models(update=args.update)
