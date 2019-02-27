a970fb3c76decd397cc885df2f2e4da85917983b
steppi <albert_steppi@hms.harvard.edu>
2019-02-27 14:06:16 -0500

    Change test classify in response to model change

--- deft/tests/test_classify.py
+++ deft/tests/test_classify.py
@@ -27,43 +27,43 @@ def test_train():
     params = {'C': 1.0,
               'ngram_range': (1, 2),
               'max_features': 1000}
-    classifier = DeftClassifier('IR', ['HGNC:6091'])
-    texts = data['train']
-    response = [label if label == 'HGNC:6091' else 'other'
-                for label in data['response']]
-    classifier.train(texts, response, **params)
+    classifier = DeftClassifier('IR', ['HGNC:6091', 'MESH:D011839'])
+    texts = data['texts']
+    labels = data['labels']
+    classifier.train(texts, labels, **params)
     assert hasattr(classifier, 'estimator')
-    assert (f1_score(response, classifier.predict(texts),
-                     pos_label='HGNC:6091') > 0.75)
+    assert (f1_score(labels, classifier.predict(texts),
+                     labels=['HGNC:6091', 'MESH:D011839'],
+                     average='weighted') > 0.5)
 
 
 @attr('slow')
 def test_cv_multiclass():
     params = {'C': [1.0],
               'max_features': [1000]}
-    classifier = DeftClassifier('IR', ['HGNC:6091'])
-    texts = data['train']
-    response = data['response']
-    classifier.cv(texts, response, param_grid=params)
-    assert classifier.best_score > 0.7
+    classifier = DeftClassifier('IR', ['HGNC:6091', 'MESH:D011839'])
+    texts = data['texts']
+    labels = data['labels']
+    classifier.cv(texts, labels, param_grid=params, cv=2)
+    assert classifier.best_score > 0.5
 
 
 @attr('slow')
 def test_cv_binary():
     params = {'C': [1.0],
               'max_features': [1000]}
-    texts = data['train']
-    response = [label if label == 'HGNC:6091' else 'other'
-                for label in data['response']]
+    texts = data['texts']
+    labels = [label if label == 'HGNC:6091' else 'ungrounded'
+              for label in data['labels']]
     classifier = DeftClassifier('IR', ['HGNC:6091'])
-    classifier.cv(texts, response, param_grid=params)
-    assert classifier.best_score > 0.7
+    classifier.cv(texts, labels, param_grid=params, cv=2)
+    assert classifier.best_score > 0.5
 
 
 def test_serialize():
     """Test that models can correctly be saved to and loaded from gzipped json
     """
-    train = data['train']
+    texts = data['texts']
     temp_filename = os.path.join(TESTS_PATH, uuid.uuid4().hex)
     classifier1 = load_model(os.path.join(MODELS_PATH, 'TEST',
                                           'test_model.gz'))
@@ -74,9 +74,9 @@ def test_serialize():
 
     classifier3 = load_model(temp_filename)
 
-    preds1, preds2, preds3 = (classifier1.predict_proba(train),
-                              classifier2.predict_proba(train),
-                              classifier3.predict_proba(train))
+    preds1, preds2, preds3 = (classifier1.predict_proba(texts),
+                              classifier2.predict_proba(texts),
+                              classifier3.predict_proba(texts))
     assert np.array_equal(preds1, preds2)
     assert np.array_equal(preds2, preds3)
     os.remove(temp_filename)
