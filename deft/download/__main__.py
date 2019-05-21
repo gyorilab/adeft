d32e1bceedefa63c3a584e65fa8543d1d77fa3c0
Albert Steppi <albert_steppi@hms.harvard.edu>
2019-05-21 14:50:06 -0400

    Set models path to live in users home directory

--- deft/download/__main__.py
+++ deft/download/__main__.py
@@ -1,4 +1,7 @@
+import os
 import argparse
+
+from deft.locations import MODELS_PATH
 from deft.download import download_models
 
 """
@@ -13,5 +16,8 @@ parser = argparse.ArgumentParser(description='Download models from S3')
 parser.add_argument('--update', action='store_true',
                     help='Update existing models if they have changed on S3')
 args = parser.parse_args()
+
+if not os.path.exists(MODELS_PATH):
+    os.makedirs(MODELS_PATH)
 
 download_models(update=args.update)
--- deft/locations.py
+++ deft/locations.py
@@ -1,5 +1,6 @@
 import os
 
 DEFT_PATH = os.path.dirname(os.path.abspath(__file__))
-MODELS_PATH = DEFT_PATH + '/models'
+MODELS_PATH = os.path.join(os.path.expanduser('~'),
+                           '.deft_models')
 S3_BUCKET_URL = 'http://deft-models.s3.amazonaws.com'
