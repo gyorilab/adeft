1aae3b0459f8d782017833e3a628010568a890da
Albert Steppi <albert_steppi@hms.harvard.edu>
2019-06-12 13:32:06 -0400

    Add comments, new options, and clean up tempfiles

--- adeft/gui/__init__.py
+++ adeft/gui/__init__.py
@@ -1,6 +1,8 @@
 import os
 import json
 import time
+import shutil
+import logging
 import tempfile
 import webbrowser
 from multiprocessing import Process
@@ -8,8 +10,52 @@ from multiprocessing import Process
 from .ground import create_app
 
 
+logger = logging.getLogger(__name__)
+
+
 def ground_with_gui(longforms, scores, grounding_map=None,
-                    names_map=None, pos_labels=None):
+                    names_map=None, pos_labels=None, verbose=False, port=5000):
+    """Opens grounding gui in browser. Returns output upon user submission
+
+    Parameters
+    ----------
+    longforms : list of str
+        List of longforms to ground
+
+    scores : list of float
+        List of scores associated to each longform
+
+    grounding_map : Optional[dict]
+        Dictionary mapping longforms to groundings. Intended for initial
+        groundings that must be manually reviewed by user, such as those
+        created by an imperfect grounding function. Default: None
+
+    names_map : Optional[dict]
+        Dictionary map groundings from grounding_map to standardized names.
+        This is ignored if grounding_map is set to None. Default: None
+
+    pos_labels : Optional[list]
+        List of groundings to be considered as positive labels.
+        This is ignored if grounding_map is set to None. Default: None
+
+    verbose : Optional[bool]
+        When true, display logging from flask's werkzeug server.
+        Default: False
+
+    port : Optional[int]
+        Port where flask is served. Defaults to flask's default.
+        Default: 5000
+
+    Returns
+    -------
+    grounding_map : dict
+        Dictionary mapping longforms to groundings
+    names : dict
+        Dictionary mapping groundings to standardized names
+    pos_labels : list
+        List of groundings to be considered as positive labels
+    """
+    # Set initial groundings etc. to empty if none are passed
     if grounding_map is None:
         grounding_map = _empty_grounding_map(longforms)
         names_map = {}
@@ -19,21 +65,35 @@ def ground_with_gui(longforms, scores, grounding_map=None,
     if pos_labels is None:
         pos_labels = []
 
+    # create temporary file for storing output
     outpath = tempfile.mkdtemp()
+    # initialize flask app
     app = create_app(longforms, scores, grounding_map,
-                     names_map, pos_labels, outpath)
-
+                     names_map, pos_labels, outpath, verbose, port)
+    # Run flask server in new process
     flask_server = Process(target=app.run)
     flask_server.start()
-    webbrowser.open('http://localhost:5000/')
+    # Open app in browser
+    webbrowser.open('http://localhost:%d/' % port)
+    # Poll until user submits groundings. Checks if output file exists
     while not os.path.exists(os.path.join(outpath, 'output.json')):
         time.sleep(1)
+    # Stop server
     flask_server.terminate()
-
+    # Get output from temporary file
     with open(os.path.join(outpath, 'output.json')) as f:
         output = json.load(f)
-    return output['grounding_map'], output['names'], output['pos_labels']
+    # Clean up temporary file
+    try:
+        shutil.rmtree(outpath)
+    except Exception:
+        logger.warning('Could not clean up temporary file %s' % outpath)
+    grounding_map = output['grounding_map']
+    names = output['names']
+    pos_labels = output['pos_labels']
+    return grounding_map, names, pos_labels
 
 
 def _empty_grounding_map(longforms):
+    """Returns empty grounding map."""
     return {longform: None for longform in longforms}
