e177aca88b2fde358614ff2e7a0463600bc0df1f
Albert Steppi <albert_steppi@hms.harvard.edu>
2019-06-17 15:36:17 -0400

    Remove exclusion of __TEST sinces files moved

--- adeft/__init__.py
+++ adeft/__init__.py
@@ -3,5 +3,4 @@ __version__ = '0.3.0'
 from adeft.download import get_available_models
 
 available_shortforms = {shortform: model
-                        for shortform, model in get_available_models().items()
-                        if model != '__TEST'}
+                        for shortform, model in get_available_models().items()}
