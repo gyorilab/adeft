8949af99733e65e699e9a8b2a5f7e5a9c2a6c20a
Albert Steppi <albert_steppi@hms.harvard.edu>
2019-06-17 17:00:07 -0400

    Exclude __TEST model

--- adeft/__init__.py
+++ adeft/__init__.py
@@ -3,4 +3,5 @@ __version__ = '0.3.0'
 from adeft.download import get_available_models
 
 available_shortforms = {shortform: model
-                        for shortform, model in get_available_models().items()}
+                        for shortform, model in get_available_models().items()
+                        if shortform != '__TEST'}
