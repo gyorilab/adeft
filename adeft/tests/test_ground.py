cd995c986423fc0020b03a84ac1fa01f3e1cfa2a
Albert Steppi <albert_steppi@hms.harvard.edu>
2019-06-19 17:10:12 -0400

    Update tests in response to changes in app state

--- adeft/tests/test_ground.py
+++ adeft/tests/test_ground.py
@@ -3,7 +3,6 @@ import tempfile
 import unittest
 
 from adeft.gui.ground import create_app
-from adeft.gui import _empty_grounding_map
 
 
 class GroundingTestCase1(unittest.TestCase):
@@ -12,8 +11,8 @@ class GroundingTestCase1(unittest.TestCase):
         longforms = ['ionizing radiation', 'insulin receptor',
                      'insulin resistance', 'irradiation']
         scores = [20.0, 15.0, 10.0, 7.0]
-        grounding_map = _empty_grounding_map(longforms)
-        names_map = {}
+        grounding_map = {longform: '' for longform in longforms}
+        names_map = {longform: '' for longform in longforms}
         pos_labels = []
         outpath = tempfile.mkdtemp()
         verbose = False
@@ -21,6 +20,9 @@ class GroundingTestCase1(unittest.TestCase):
         app = create_app(longforms, scores, grounding_map, names_map,
                          pos_labels, outpath, verbose, port)
         app.testing = True
+        self.longforms = longforms
+        self.grounding_map = grounding_map
+        self.names_map = names_map
         self.app = app
 
     # Tests
@@ -29,105 +31,124 @@ class GroundingTestCase1(unittest.TestCase):
         with self.app.test_client() as tc:
             res = tc.get('/')
             assert res.status_code == 200, res
-            assert flask.session['names'] == ['', '', '', '']
-            assert flask.app.session['groundings'] == ['', '', '', '']
+
+            assert flask.session['names_map'] == self.names_map
+            assert flask.session['grounding_map'] == self.grounding_map
 
     def test_add_groundings1(self):
         """Test adding a single name, grounding pair"""
         with self.app.test_client() as tc:
             res = tc.get('/')
             assert res.status_code == 200, res
+
+            names_map = self.names_map.copy()
+            grounding_map = self.grounding_map.copy()
+
             res = tc.post('ground_add',
                           data={'name': 'INSR',
                                 'grounding': 'HGNC:6091',
-                                'select': '2'})
+                                'select': '1'})
             assert res.status_code == 200, res
-            assert flask.session['names'] == ['', 'INSR', '', '']
-            assert flask.session['groundings'] == ['', 'HGNC:6091', '', '']
+            names_map['insulin receptor'] = 'INSR'
+            grounding_map['insulin receptor'] = 'HGNC:6091'
+          
+            assert names_map == flask.session['names_map']
+            assert grounding_map == flask.session['grounding_map']
 
     def test_add_groundings2(self):
         """Test adding same name and grounding to multiple longforms."""
         with self.app.test_client() as tc:
             res = tc.get('/')
             assert res.status_code == 200, res
+
+            names_map = self.names_map.copy()
+            grounding_map = self.grounding_map.copy()
+
             res = tc.post('ground_add',
                           data={'name': 'Radiation, Ionizing',
                                 'grounding': 'MESH:D011839',
-                                'select': ['1', '4']})
+                                'select': ['0', '3']})
             assert res.status_code == 200, res
-            assert flask.session['names'] == ['Radiation, Ionizing', '', '',
-                                              'Radiation, Ionizing']
-            assert flask.session['groundings'] == ['MESH:D011839', '', '',
-                                                   'MESH:D011839']
+
+            names_map['ionizing radiation'] = 'Radiation, Ionizing'
+            names_map['irradiation'] = 'Radiation, Ionizing'
+            grounding_map['ionizing radiation'] = 'MESH:D011839'
+            grounding_map['irradiation'] = 'MESH:D011839'
+
+            assert names_map == flask.session['names_map']
+            assert grounding_map == flask.session['grounding_map']
 
     def test_add_groundings3(self):
         """Test entering successive groundings."""
         with self.app.test_client() as tc:
             tc.get('/')
+            names_map = self.names_map.copy()
+            grounding_map = self.grounding_map.copy()
             res = tc.post('ground_add',
                           data={'name': 'Radiation, Ionizing',
                                 'grounding': 'MESH:D011839',
-                                'select': ['1', '4']})
+                                'select': ['0', '3']})
             assert res.status_code == 200, res
-
-            # Add incorrect grounding for insulin receptor
             res = tc.post('ground_add',
                           data={'name': 'INSR',
                                 'grounding': 'HGNC:6090',
-                                'select': '2'})
+                                'select': '1'})
             assert res.status_code == 200, res
-            assert flask.session['names'] == ['Radiation, Ionizing', 'INSR',
-                                              '', 'Radiation, Ionizing']
-            assert flask.session['groundings'] == ['MESH:D011839', 'HGNC:6090',
-                                                   '', 'MESH:D011839']
-            # resubmit with correct grounding
+
+            names_map['ionizing radiation'] = 'Radiation, Ionizing'
+            names_map['irradiation'] = 'Radiation, Ionizing'
+            names_map['insulin receptor'] = 'INSR'
+            grounding_map['ionizing radiation'] = 'MESH:D011839'
+            grounding_map['irradiation'] = 'MESH:D011839'
+            grounding_map['insulin receptor'] = 'HGNC:6090'
+
+            assert names_map == flask.session['names_map']
+            assert grounding_map == flask.session['grounding_map']
+
             res = tc.post('ground_add',
                           data={'name': 'INSR',
                                 'grounding': 'HGNC:6091',
-                                'select': '2'})
+                                'select': '1'})
             assert res.status_code == 200, res
-            assert flask.session['names'] == ['Radiation, Ionizing', 'INSR',
-                                              '', 'Radiation, Ionizing']
-            assert flask.session['groundings'] == ['MESH:D011839', 'HGNC:6091',
-                                                   '', 'MESH:D011839']
+
+            grounding_map['insulin receptor'] = 'HGNC:6091'
+            assert grounding_map == flask.session['grounding_map']
 
     def test_add_and_delete_groundings(self):
         """Test deletion of groundings"""
         with self.app.test_client() as tc:
             tc.get('/')
+            names_map = self.names_map.copy()
+            grounding_map = self.grounding_map.copy()
             res = tc.post('ground_add',
                           data={'name': 'Radiation, Ionizing',
                                 'grounding': 'MESH:D011839',
-                                'select': ['1', '4']})
+                                'select': ['0', '3']})
             assert res.status_code == 200, res
 
             res = tc.post('ground_add',
                           data={'name': 'INSR',
                                 'grounding': 'HGNC:6090',
-                                'select': '2'})
+                                'select': '1'})
             assert res.status_code == 200, res
-            assert flask.session['names'] == ['Radiation, Ionizing', 'INSR',
-                                              '', 'Radiation, Ionizing']
-            assert flask.session['groundings'] == ['MESH:D011839', 'HGNC:6090',
-                                                   '', 'MESH:D011839']
 
-            # Delete incorrect grounding for insulin receptor
             res = tc.post('ground_delete',
-                          data={'delete.2': 'X'})
+                          data={'delete.1': 'X'})
             assert res.status_code == 200, res
 
-            assert flask.session['names'] == ['Radiation, Ionizing', '', '',
-                                              'Radiation, Ionizing']
-            assert flask.session['groundings'] == ['MESH:D011839', '', '',
-                                                   'MESH:D011839']
+            names_map['ionizing radiation'] = 'Radiation, Ionizing'
+            names_map['irradiation'] = 'Radiation, Ionizing'
+            grounding_map['ionizing radiation'] = 'MESH:D011839'
+            grounding_map['irradiation'] = 'MESH:D011839'
+
+            assert names_map == flask.session['names_map']
+            assert grounding_map == flask.session['grounding_map']
 
             # test that deletion does nothing when there are no groundings
             res = tc.post('ground_delete',
-                          data={'delete.3': 'X'})
-            assert flask.session['names'] == ['Radiation, Ionizing', '', '',
-                                              'Radiation, Ionizing']
-            assert flask.session['groundings'] == ['MESH:D011839', '', '',
-                                                   'MESH:D011839']
+                          data={'delete.2': 'X'})
+            assert names_map == flask.session['names_map']
+            assert grounding_map == flask.session['grounding_map']
 
     def test_set_pos_labels(self):
         """Test setting of positive labels"""
@@ -138,47 +159,40 @@ class GroundingTestCase1(unittest.TestCase):
             res = tc.post('ground_add',
                           data={'name': 'Radiation, Ionizing',
                                 'grounding': 'MESH:D011839',
-                                'select': ['1', '4']})
+                                'select': ['0', '3']})
             assert res.status_code == 200, res
 
             res = tc.post('ground_add',
                           data={'name': 'INSR',
                                 'grounding': 'HGNC:6091',
-                                'select': '2'})
+                                'select': '1'})
             assert res.status_code == 200, res
 
             res = tc.post('ground_add',
                           data={'name': 'Insulin Resistance',
                                 'grounding': 'MESH:D007333',
-                                'select': '3'})
-                          
-            assert res.status_code == 200, res
-            assert flask.session['names'] == ['Radiation, Ionizing', 'INSR',
-                                              'Insulin Resistance',
-                                              'Radiation, Ionizing']
-            assert flask.session['groundings'] == ['MESH:D011839', 'HGNC:6091',
-                                                   'MESH:D007333',
-                                                   'MESH:D011839']
+                                'select': '2'})
 
             # Labels are the unique groundings stored in sorted order
             # pos labels contains a list of strings representing ints
             res = tc.post('ground_pos_label',
-                          data={'pos-label.1': '+'})
+                          data={'pos-label.0': '+'})
             assert res.status_code == 200, res
+
             assert flask.session['pos_labels'] == [0]
 
             res = tc.post('ground_pos_label',
-                          data={'pos-label.2': '+'})
+                          data={'pos-label.1': '+'})
             assert res.status_code == 200, res
             assert flask.session['pos_labels'] == [0, 1]
 
             res = tc.post('ground_pos_label',
-                          data={'pos-label.3': '+'})
+                          data={'pos-label.2': '+'})
             assert res.status_code == 200, res
             assert flask.session['pos_labels'] == [0, 1, 2]
 
             res = tc.post('ground_pos_label',
-                          data={'pos-label.2': '+'})
+                          data={'pos-label.1': '+'})
             assert res.status_code == 200, res
             assert flask.session['pos_labels'] == [0, 2]
 
@@ -189,23 +203,27 @@ class GroundingTestCase1(unittest.TestCase):
             res = tc.post('ground_add',
                           data={'name': 'Radiation, Ionizing',
                                 'grounding': 'MESH:D011839',
-                                'select': ['1', '4']})
+                                'select': ['0', '3']})
             assert res.status_code == 200, res
 
             res = tc.post('ground_add',
                           data={'name': 'INSR',
                                 'grounding': 'HGNC:6091',
-                                'select': '2'})
+                                'select': '1'})
             assert res.status_code == 200, res
 
             res = tc.post('ground_add',
                           data={'name': 'Insulin Resistance',
                                 'grounding': 'MESH:D007333',
-                                'select': '3'})
+                                'select': '2'})
 
             assert res.status_code == 200, res
             # Labels are the unique groundings stored in sorted order
             # pos labels contains a list of strings representing ints
+            res = tc.post('ground_pos_label',
+                          data={'pos-label.0': '+'})
+            assert res.status_code == 200, res
+
             res = tc.post('ground_pos_label',
                           data={'pos-label.1': '+'})
             assert res.status_code == 200, res
@@ -213,36 +231,25 @@ class GroundingTestCase1(unittest.TestCase):
             res = tc.post('ground_pos_label',
                           data={'pos-label.2': '+'})
             assert res.status_code == 200, res
-
-            res = tc.post('ground_pos_label',
-                          data={'pos-label.3': '+'})
-            assert res.status_code == 200, res
+            assert flask.session['pos_labels'] == [0, 1, 2]
 
             res = tc.post('ground_delete',
-                          data={'delete.3': 'X'})
+                          data={'delete.2': 'X'})
             assert res.status_code == 200, res
             assert flask.session['pos_labels'] == [0, 1]
 
             res = tc.post('ground_add',
                           data={'name': 'Insulin Resistance',
                                 'grounding': 'MESH:D007332',
-                                'select': '3'})
+                                'select': '2'})
             assert flask.session['pos_labels'] == [0, 2]
 
             res = tc.post('ground_pos_label',
-                          data={'pos-label.2': '+'})
+                          data={'pos-label.1': '+'})
             assert flask.session['pos_labels'] == [0, 1, 2]
 
             tc.post('ground_add',
                     data={'name': 'Insulin Resistance',
                           'grounding': 'MESH:D007333',
-                          'select': '3'})
-            print(flask.session['pos_labels'])
-            assert flask.session['pos_labels'] == [0, 1, 2]
-
-            tc.post('ground_add',
-                    data={'name': 'Insulin Resistance',
-                          'grounding': 'MESH:D999999',
-                          'select': '3'})
-            print(flask.session['pos_labels'])
-            assert flask.session['pos_labels'] == [0, 1, 2]
+                          'select': '2'})
+            assert flask.session['pos_labels'] == [0, 2]
