cfa9d6fa9a103b2047d156a2ab9104272976f5a9
Albert Steppi <albert_steppi@hms.harvard.edu>
2019-06-19 17:09:22 -0400

    Change structure of info passed to app

--- adeft/gui/__init__.py
+++ adeft/gui/__init__.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 
 def ground_with_gui(longforms, scores, grounding_map=None,
-                    names_map=None, pos_labels=None, verbose=False, port=5000):
+                    names=None, pos_labels=None, verbose=False, port=5000):
     """Opens grounding gui in browser. Returns output upon user submission
 
     Parameters
@@ -27,7 +27,7 @@ def ground_with_gui(longforms, scores, grounding_map=None,
         groundings that must be manually reviewed by user, such as those
         created by an imperfect grounding function. Default: None
 
-    names_map : Optional[dict]
+    names : Optional[dict]
         Dictionary map groundings from grounding_map to standardized names.
         This is ignored if grounding_map is set to None. Default: None
 
@@ -55,13 +55,30 @@ def ground_with_gui(longforms, scores, grounding_map=None,
     from .ground import create_app
     # Set initial groundings etc. to empty if none are passed
     if grounding_map is None:
-        grounding_map = _empty_grounding_map(longforms)
-        names_map = {}
+        grounding_map = {longform: '' for longform in longforms}
+        names_map = {longform: '' for longform in longforms}
         pos_labels = []
-    elif names_map is None:
-        names_map = {}
+    elif names is None:
+        names_map = {longform: '' for longform in longforms}
+    else:
+        if not set(names_map.keys()) <= set(grounding_map.values()):
+            raise ValueError('keys in names_map must be subset of values of'
+                             ' grounding_map')
+        grounding_map = {longform: grounding_map[longform]
+                         if longform in grounding_map
+                         and grounding_map[longform]
+                         else '' for longform in longforms}
+        names_map = {longform: names_map[grounding_map[longform]]
+                     if longform in grounding_map and
+                     grounding_map[longform] in names_map
+                     and names_map[grounding_map[longform]] else ''
+                     for longform in longforms}
     if pos_labels is None:
         pos_labels = []
+
+    if not set(names_map.keys()) <= set(grounding_map.values()):
+        raise ValueError('keys in names_map must be subset of values of'
+                         ' grounding_map')
 
     # create temporary file for storing output
     outpath = tempfile.mkdtemp()
@@ -86,12 +103,8 @@ def ground_with_gui(longforms, scores, grounding_map=None,
         shutil.rmtree(outpath)
     except Exception:
         logger.warning('Could not clean up temporary file %s' % outpath)
+
     grounding_map = output['grounding_map']
     names = output['names']
     pos_labels = output['pos_labels']
     return grounding_map, names, pos_labels
-
-
-def _empty_grounding_map(longforms):
-    """Returns empty grounding map."""
-    return {longform: None for longform in longforms}
