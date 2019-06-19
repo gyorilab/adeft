264ba312956434217ac1a28200741e7d66885163
Albert Steppi <albert_steppi@hms.harvard.edu>
2019-06-19 17:07:02 -0400

    Write class for storing state needed in page

--- adeft/gui/ground/ground.py
+++ adeft/gui/ground/ground.py
@@ -119,3 +119,56 @@ def generate_grounding_map():
         json.dump(output, f)
 
     return "Groundings Submitted."
+
+
+class GroundingState(object):
+    """Stores state of current page
+
+    This class is initialized from session variables and contains
+    methods for transforming state in response to user input.
+    """
+    def __init__(self, longforms, grounding_map, names_map,
+                 labels, pos_labels):
+        self.longforms = longforms
+        self.grounding_map = grounding_map
+        self.names_map = names_map
+        self.labels = labels
+        self.pos_labels = pos_labels
+
+    def dump(self):
+        """Returns state needed to reload page."""
+        return self.grounding_map, self.names_map, self.labels, self.pos_labels
+
+    def add(self, name, grounding, row_numbers):
+        """Add new names and groundings"""
+        positive_groundings = set(self.labels[index]
+                                  for index in self.pos_labels)
+        for i in row_numbers:
+            if grounding:
+                self.grounding_map[self.longforms[i]] = grounding
+            if name:
+                self.names_map[self.longforms[i]] = name
+
+        labels = sorted(set(grounding for _, grounding in
+                            self.grounding_map.items()))
+        pos_labels = [i for i, label in enumerate(labels)
+                      if label in positive_groundings]
+        self.labels = labels
+        self.pos_labels = pos_labels
+
+    def delete(self, row_number):
+        """Delete names and groundings"""
+        longform = self.longforms[row_number]
+        grounding = self.grounding_map[longform]
+        self.grounding_map[longform] = self.names_map[longform] = ''
+        if grounding not in self.grounding_map.values():
+            label_number = self.labels.index(grounding)
+            self.pos_labels = [i if i < label_number else i - 1
+                               for i in self.pos_labels if i != label_number]
+            self.labels = (self.labels[:label_number] +
+                           self.labels[label_number+1:])
+
+    def toggle_positive(self, label_number):
+        """Toggle labels as positive or negative"""
+        pos_labels = sorted(set(self.pos_labels) ^ set([label_number]))
+        self.pos_labels = pos_labels
