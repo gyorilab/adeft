da65d83326c79ff546eab77b7fa97b814249d544
Albert Steppi <albert_steppi@hms.harvard.edu>
2019-06-19 17:09:53 -0400

    Make template access session variables directly

--- adeft/gui/ground/__init__.py
+++ adeft/gui/ground/__init__.py
@@ -31,29 +31,14 @@ def create_app(longforms, scores,
     @app.route('/')
     def initialize():
         """Load initial page for grounding app"""
-        groundings = [grounding_map[longform] for longform in longforms]
-        names = [names_map[grounding] if grounding is not None else None
-                 for grounding in groundings]
-        labels = _get_labels(groundings)
-        groundings = [grounding if grounding is not None else ''
-                      for grounding in groundings]
-        names = [name if name is not None else '' for name in names]
+        labels = sorted(grounding for _, grounding in grounding_map.items())
 
-        session['groundings'] = groundings
-        session['names'] = names
+        session['grounding_map'] = grounding_map
+        session['names_map'] = names_map
         session['labels'] = labels
         session['pos_labels'] = pos_labels
 
-        data = list(zip(longforms, scores, names, groundings, labels))
-        return render_template('input.jinja2', data=data,
-                               pos_labels=pos_labels)
+        return render_template('input.jinja2')
 
     app.register_blueprint(ground.bp)
     return app
-
-
-def _get_labels(groundings):
-    """Get list of labels for table from current groundings"""
-    labels = sorted(set(grounding for grounding in groundings if grounding))
-    labels.extend(['']*(len(groundings) - len(labels)))
-    return labels
