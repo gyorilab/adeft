9594b3943db40746b7e148d8eae1cc6d8fcdd2a5
Albert Steppi <albert_steppi@hms.harvard.edu>
2019-07-19 17:01:49 -0400

    Print full output with wildcards included

--- adeft/score/score_util.pyx
+++ adeft/score/score_util.pyx
@@ -224,7 +224,8 @@ def check_convert():
     output = stitch(&candidates, perm, 3)
 
     print(output[0], output[1], output[2], output[3], output[4],
-          output[5])
+          output[5], output[6], output[7], output[8], output[9],
+          output[10], output[12])
 
     free_candidates_array(&candidates)            
 
