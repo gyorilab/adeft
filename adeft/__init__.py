c57d52b669d4839ce5ab92c3ce727a2db1729a91
steppi <albert_steppi@hms.harvard.edu>
2019-11-08 18:01:05 -0500

    Update version numbers

--- adeft/__init__.py
+++ adeft/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.5.0'
+__version__ = '0.5.1'
 
 from adeft.download import get_available_models
 
--- doc/conf.py
+++ doc/conf.py
@@ -26,7 +26,7 @@ author = 'Albert Steppi'
 # The short X.Y version
 version = '0.5'
 # The full version, including alpha/beta/rc tags
-release = '0.5.0'
+release = '0.5.1'
 
 
 # -- General configuration ---------------------------------------------------
