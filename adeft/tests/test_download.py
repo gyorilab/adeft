42887d7acc83ebbc6cb833c33197061a5fe73e85
steppi <albert_steppi@hms.harvard.edu>
2020-01-24 19:10:38 -0500

    Fix name of function in test

--- adeft/tests/test_download.py
+++ adeft/tests/test_download.py
@@ -3,7 +3,7 @@ import shutil
 
 from adeft.locations import ADEFT_MODELS_PATH, TEST_RESOURCES_PATH
 from adeft.download import download_models, get_available_models, \
-    get_s3_models, download_test_resources, setup_test_resource_folders
+    get_s3_models, download_test_resources, setup_test_resource_folder
 
 
 def test_get_s3_models():
@@ -27,7 +27,7 @@ def test_download_models():
 
 def test_download_test_resources():
     """Test download of test resources from S3"""
-    setup_test_resource_folders()
+    setup_test_resource_folder()
     test_model_path = os.path.join(TEST_RESOURCES_PATH, 'test_model', 'IR')
     assert os.path.exists(os.path.join(test_model_path, 'IR_model.gz'))
     assert os.path.exists(os.path.join(test_model_path, 'IR_names.json'))
