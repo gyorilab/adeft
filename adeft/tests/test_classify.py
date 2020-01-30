c41929b9a2af66ec1e97e282ce8bd642fc60ad5c
Albert Steppi <albert_steppi@hms.harvard.edu>
2020-01-30 10:28:54 -0500

    Remove platform dependent tests

--- adeft/tests/test_classify.py
+++ adeft/tests/test_classify.py
@@ -31,84 +31,40 @@ def test_train():
                                  random_state=1729)
     texts = data['texts']
     labels = data['labels']
-    expected_f1 = 0.7759358447458177
-    expected_fimp = {'HGNC:6091': [('irs', 0.6320),
-                                   ('igf', 0.6245),
-                                   ('insulin', 0.6107),
-                                   ('cell', 0.0),
-                                   ('cells', 0.0),
-                                   ('expression', 0.0),
-                                   ('levels', 0.0),
-                                   ('study', 0.0),
-                                   ('group', -0.2162),
-                                   ('induced', -0.3138)],
-                     'MESH:D011839': [('cells', 0.4574),
-                                      ('induced', 0.3435),
-                                      ('cell', 0.2507),
-                                      ('expression', 0.0),
-                                      ('group', 0.0),
-                                      ('igf', 0.0),
-                                      ('irs', 0.0),
-                                      ('study', 0.0),
-                                      ('levels', -0.2277),
-                                      ('insulin', -1.8653)],
-                     'ungrounded': [('study', 0.4991),
-                                    ('group', 0.4120),
-                                    ('expression', 0.0),
-                                    ('induced', 0.0),
-                                    ('insulin', 0.0),
-                                    ('irs', 0.0),
-                                    ('levels', 0.0),
-                                    ('cell', -0.0399),
-                                    ('igf', -0.0579),
-                                    ('cells', -0.9570)]}
     classifier.train(texts, labels, **params)
     assert hasattr(classifier, 'estimator')
     assert (f1_score(labels, classifier.predict(texts),
                      labels=['HGNC:6091', 'MESH:D011839'],
-                     average='weighted') - expected_f1 < 1e-10)
-    assert classifier.feature_importances() == expected_fimp
+                     average='weighted') > 0.5)
+    importances = classifier.feature_importances()
+    INSR_features, INSR_scores = zip(*importances['HGNC:6091'])
+    assert set(['irs', 'igf', 'insulin']) < set(INSR_features)
+    irs_score = [score for feature, score in importances['HGNC:6091']
+                 if feature == 'irs'][0]
+    assert irs_score > 0
+    # test that results are repeatable
+    coef1 = classifier.estimator.named_steps['logit'].coef_
+    classifier.train(texts, labels, **params)
+    coef2 = classifier.estimator.named_steps['logit'].coef_
+    assert np.array_equal(coef1, coef2)
 
 
 @attr('slow')
 def test_cv_multiclass():
     params = {'C': [1.0],
               'max_features': [10]}
-    # Classification results are now reproducible. We should be aware of any
-    # modifications that will change these results.
-    expected_stats = {'label_distribution': {'HGNC:6091': 101,
-                                             'MESH:D011839': 173,
-                                             'ungrounded': 226},
-                      'f1': {'mean': 0.704288,
-                             'std': 0.049344},
-                      'precision': {'mean': 0.784593,
-                                    'std': 0.011247},
-                      'recall': {'mean': 0.661125,
-                                 'std': 0.074169},
-                      'ungrounded': {'f1': {'mean': 0.744231,
-                                            'std': 0.005769},
-                                     'pr': {'mean': 0.809735,
-                                            'std': 0.039823},
-                                     'rc': {'mean': 0.692077,
-                                            'std': 0.039016}},
-                      'MESH:D011839': {'f1': {'mean': 0.782527,
-                                              'std': 0.048799},
-                                       'pr': {'mean': 0.757485,
-                                              'std': 0.044841},
-                                       'rc': {'mean': 0.809299,
-                                              'std': 0.053201}},
-                      'HGNC:6091': {'f1': {'mean': 0.570274,
-                                           'std': 0.049726},
-                                    'pr': {'mean': 0.496275,
-                                           'std': 0.123725},
-                                    'rc': {'mean': 0.741818,
-                                           'std': 0.121818}}}
     classifier = AdeftClassifier('IR', ['HGNC:6091', 'MESH:D011839'],
                                  random_state=1729)
     texts = data['texts']
     labels = data['labels']
     classifier.cv(texts, labels, param_grid=params, cv=2)
-    assert classifier.stats == expected_stats
+    assert classifier.stats['f1']['mean'] > 0.5
+    assert classifier.stats['ungrounded']['f1']['mean'] > 0.5
+    # Test that results are repeatable
+    coef1 = classifier.estimator.named_steps['logit'].coef_
+    classifier.cv(texts, labels, param_grid=params, cv=2)
+    coef2 = classifier.estimator.named_steps['logit'].coef_
+    assert np.array_equal(coef1, coef2)
 
 
 @attr('slow')
@@ -116,52 +72,21 @@ def test_cv_binary():
     params = {'C': [1.0],
               'max_features': [10]}
     texts = data['texts']
-    expected_stats = {'label_distribution': {'HGNC:6091': 101,
-                                             'ungrounded': 399},
-                      'f1': {'mean': 0.536613,
-                             'std': 0.115561},
-                      'precision': {'mean': 0.677143,
-                                    'std': 0.037143},
-                      'recall': {'mean': 0.456863,
-                                 'std': 0.143137},
-                      'HGNC:6091': {'f1': {'mean': 0.536613,
-                                           'std': 0.115561},
-                                    'pr': {'mean': 0.456863,
-                                           'std': 0.143137},
-                                    'rc': {'mean': 0.677143,
-                                           'std': 0.037143}},
-                      'ungrounded': {'f1': {'mean': 0.908948,
-                                            'std': 0.012234},
-                                     'pr': {'mean': 0.947349,
-                                            'std': 0.007651},
-                                     'rc': {'mean': 0.874257,
-                                            'std': 0.029124}}}
-    expected_fi = {'ungrounded': [('group', 0.6063),
-                                  ('induced', 0.4483),
-                                  ('study', 0.2721),
-                                  ('cell', 0.0),
-                                  ('expression', -0.0017),
-                                  ('levels', -0.0187),
-                                  ('cells', -0.0761),
-                                  ('irs', -0.6719),
-                                  ('igf', -0.7052),
-                                  ('insulin', -0.7869)],
-                   'HGNC:6091': [('insulin', 0.7869),
-                                 ('igf', 0.7052),
-                                 ('irs', 0.6719),
-                                 ('cells', 0.0761),
-                                 ('levels', 0.0187),
-                                 ('expression', 0.0017),
-                                 ('cell', -0.0),
-                                 ('study', -0.2721),
-                                 ('induced', -0.4483),
-                                 ('group', -0.6063)]}
     labels = [label if label == 'HGNC:6091' else 'ungrounded'
               for label in data['labels']]
     classifier = AdeftClassifier('IR', ['HGNC:6091'], random_state=1729)
     classifier.cv(texts, labels, param_grid=params, cv=2)
-    assert classifier.stats == expected_stats
-    assert classifier.feature_importances() == expected_fi
+    assert classifier.stats['f1']['mean'] > 0.5
+    assert classifier.stats['HGNC:6091']['f1']['mean'] > 0.5
+    importances = classifier.feature_importances()
+    INSR_features, INSR_scores = zip(*importances['HGNC:6091'])
+    ungrounded_features, ungrounded_scores = zip(*importances['ungrounded'])
+    assert set(INSR_features) == set(ungrounded_features)
+    assert INSR_scores == tuple(-x for x in ungrounded_scores[::-1])
+    assert [score for feature, score in importances['HGNC:6091']
+            if feature == 'insulin'][0] > 0
+    assert [score for feature, score in importances['HGNC:6091']
+            if feature == 'group'][0] < 0
 
 
 def test_training_set_digest():
