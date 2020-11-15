e5dc1e3a61c4d99f00edfb3dc7c5cb1be8ec52e4
steppi <albert_steppi@hms.harvard.edu>
2020-11-15 18:33:31 -0500

    Remove unused RESOURCES_PATH from locations

--- adeft/locations.py
+++ adeft/locations.py
@@ -15,6 +15,5 @@ if ADEFT_HOME is None:
 
 ADEFT_PATH = os.path.join(ADEFT_HOME, __version__)
 ADEFT_MODELS_PATH = os.path.join(ADEFT_PATH, 'models')
-RESOURCES_PATH = os.path.join(ADEFT_PATH, 'resources')
 TEST_RESOURCES_PATH = os.path.join(ADEFT_PATH, 'test_resources')
 S3_BUCKET_URL = os.path.join('http://adeft.s3.amazonaws.com', __version__)
