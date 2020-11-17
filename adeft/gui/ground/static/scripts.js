a48c7b6e1279c0fd02160b570d076172b5b17500
steppi <albert_steppi@hms.harvard.edu>
2020-11-17 17:45:34 -0500

    Keep text box input on page update

--- adeft/gui/ground/static/scripts.js
+++ adeft/gui/ground/static/scripts.js
@@ -4,10 +4,35 @@ document.addEventListener("DOMContentLoaded", function (event) {
         window.scrollTo(0, scrollpos);
         sessionStorage.removeItem('scrollpos');
     }
+    var namespaceBoxContent = sessionStorage.getItem('namespace-box-content');
+    var nameBoxContent = sessionStorage.getItem('name-box-content');
+    var identifierBoxContent = sessionStorage.getItem('identifier-box-content');
+    if (namespaceBoxContent) {
+	document.getElementById('namespace-box').value = namespaceBoxContent;
+	sessionStorage.removeItem('namespace-box-content');
+    }
+    if (nameBoxContent) {
+	document.getElementById('name-box').value = nameBoxContent;
+	sessionStorage.removeItem('name-box-content');
+    }
+    if (identifierBoxContent) {
+	document.getElementById('identifier-box').value = identifierBoxContent;
+	sessionStorage.removeItem('identifier-box-content');
+    }
 });
 
 function setScrollPosition() {
-  sessionStorage.setItem('scrollpos', window.scrollY);
+    sessionStorage.setItem('scrollpos', window.scrollY);
+}
+
+function setScrollPositionAndInputBoxContent() {
+    sessionStorage.setItem('scrollpos', window.scrollY);
+    sessionStorage.setItem('namespace-box-content',
+			   document.getElementById('namespace-box').value);
+    sessionStorage.setItem('name-box-content',
+			   document.getElementById('name-box').value);
+    sessionStorage.setItem('identifier-box-content',
+			   document.getElementById('identifier-box').value);
 }
 
 
@@ -20,20 +45,20 @@ window.onload = function() {
 	    grounding = event.target.getAttribute('data-grounding');
 	    split_grounding = grounding.split(':')
 	    if (split_grounding.length == 3) {
-		namespace = split_grounding[0];
-		identifier = split_grounding[1] + ':' + split_grounding[2];
+		var namespace = split_grounding[0];
+		var identifier = split_grounding[1] + ':' + split_grounding[2];
 	    }
 	    else if (split_grounding.length == 2) {
-		namespace = split_grounding[0];
-		identifier = split_grounding[1];
+		var namespace = split_grounding[0];
+		var identifier = split_grounding[1];
 	    }
 	    else if (split_grounding.length == 1) {
-		namespace = '';
-		identifier = split_grounding[0];
+		var namespace = '';
+		var identifier = split_grounding[0];
 	    }
 	    else {
-		namespace = '';
-		identifier = '';
+		var namespace = '';
+		var identifier = '';
 	    }
 	    document.getElementById('namespace-box').value = namespace;
 	    document.getElementById('identifier-box').value = identifier;
