194d1ba66f100ace0b125f0980c32d9d96d8a3ee
steppi <albert_steppi@hms.harvard.edu>
2021-03-24 14:31:38 -0400

    Hardcode S3_BUCKET_URL
    It is not platform dependent

--- adeft/locations.py
+++ adeft/locations.py
@@ -20,4 +20,4 @@ ADEFT_MODELS_PATH = os.path.join(ADEFT_PATH, 'models')
 RESOURCES_PATH = os.path.join(ADEFT_PATH, 'resources')
 GROUNDINGS_FILE_PATH = os.path.join(RESOURCES_PATH, 'groundings.csv')
 TEST_RESOURCES_PATH = os.path.join(ADEFT_PATH, 'test_resources')
-S3_BUCKET_URL = os.path.join('http://adeft.s3.amazonaws.com', __version__)
+S3_BUCKET_URL = f'https://adeft.s3.amazonaws.com/{__version__}'
