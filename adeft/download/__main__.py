8ddf336418a609cb76f51aa7d24d665d1a617d86
steppi <albert_steppi@hms.harvard.edu>
2021-05-06 21:31:44 -0400

    Use appdirs to choose location of adeft home folder

--- adeft/download/__main__.py
+++ adeft/download/__main__.py
@@ -5,7 +5,7 @@ from adeft.download import setup_models_folder, setup_resources_folder, \
     setup_test_resource_folder
 
 
-# Create .adeft folder if it does not already exist
+# Create adeft data folder if it does not already exist
 if not os.path.exists(ADEFT_PATH):
     os.makedirs(ADEFT_PATH)
 
