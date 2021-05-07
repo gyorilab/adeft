ff7a4a6c5387cd45424e3f879e8de457228a18d0
steppi <albert_steppi@hms.harvard.edu>
2021-05-06 22:55:14 -0400

    Update module docstring for new home location

--- adeft/locations.py
+++ adeft/locations.py
@@ -1,8 +1,9 @@
 """
 Contains paths to locations on user's system where models and resources are
-to be stored. These all live in adeft's home folder which defaults to the
-hidden directory ".adeft" in the user's home directory but which can be
-specified by setting the environment variable ADEFT_HOME in the user's profile.
+to be stored. These all live in adeft's home folder which defaults to
+a directory "adeft" in a location determined by Python's `appdirs` package.
+An alternative location can be specified by setting the environment variable
+ADEFT_HOME in the user's profile.
 """
 
 import os
